352efb84a13b96713be7c1c2605583a420cd7dd4
rupertbg <input@rupertbg.nz>
2016-01-16 23:08:51 +1300

    Squashed scrolling bugs on smaller screens
    Also added css class to passthrough active state between fixed and
    floating navigation.

--- scripts/anchor-scroll.js
+++ scripts/anchor-scroll.js
@@ -1,86 +1,4 @@
-// Anchor position and smooth scrolling
-
-(function(document, history, location) {
-  var HISTORY_SUPPORT = !!(history && history.pushState);
-
-  var anchorScrolls = {
-    ANCHOR_REGEX: /^#[^ ]+$/,
-
-    /**
-     * Establish events, and fix initial scroll position if a hash is provided.
-     */
-    
-    init: function() {
-      this.scrollIfAnchor(location.hash);
-      $('body').on('click', 'a', $.proxy(this, 'delegateAnchors'));
-    },
-
-    /**
-     * If the provided href is an anchor which resolves to an element on the
-     * page, scroll to it.
-     * @param  {String} href
-     * @return {Boolean} - Was the href an anchor.
-     */
-    
-    scrollIfAnchor: function(href, pushToHistory) {
-      var match, anchorOffset;
-
-      if(!this.ANCHOR_REGEX.test(href)) {
-        return false;
-      }
-
-      match = document.getElementById(href.slice(1));
-
-      if(match) {
-        anchorOffset = $(match).offset().top - $('.sticky-navbar').outerHeight();
-        $('html, body').animate({ scrollTop: anchorOffset});
-
-        // Add the state to history as-per normal anchor links
-        if(HISTORY_SUPPORT && pushToHistory) {
-          history.pushState({}, document.title, location.pathname + href);
-        }
-      }
-
-      return !!match;
-    },
-
-    /**
-     * If the click event's target was an anchor, fix the scroll position.
-     */
-    
-    delegateAnchors: function(e) {
-      var elem = e.target;
-
-      if(this.scrollIfAnchor(elem.getAttribute('href'), true)) {
-        e.preventDefault();
-      }
-    }
-  };
-
-	$(document).ready($.proxy(anchorScrolls, 'init'));
-})(window.document, window.history, window.location);
-
-
-// Smoother scrolling when interacting with above
-
-$(document).ready(function(){
-	$('a[href^="#"]').on('click',function (e) {
-	    e.preventDefault();
-
-	    var target = this.hash;
-	    var $target = $(target) - $('.sticky-navbar').outerHeight();
-
-	    $('html, body').stop().animate({
-	        'scrollTop': $target.offset().top
-	    }, 100, 'swing');
-	});
-});
-
-// Navigation active selector 
-
-$.fn.scrollBottom = function() { 
-  return $(document).height() - this.scrollTop() - this.height(); 
-};
+// Navigation anchor position & active selector 
 
 $(document).ready(function () {
 		$(document).on("scroll", onScroll);
@@ -97,8 +15,8 @@ $(document).ready(function () {
 			var target = this.hash;
 			$target = $(target);
 			$('html, body').stop().animate({
-				'scrollTop': $target.offset().top+2
-			}, 500, 'swing', function () {
+				'scrollTop': $target.offset().top-$('.sticky-navbar').outerHeight()
+			}, 400, 'swing', function () {
 				window.location.hash = target;
 				$(document).on("scroll", onScroll);
 			});
@@ -108,11 +26,11 @@ $(document).ready(function () {
 	function onScroll(event){
 		var scrollPosition = $(window).scrollTop();
         var navbarHeight = $('.sticky-navbar').outerHeight() + 1;
-		$('.sticky-nav a').each(function () {
+		$('.nav-highlight a').each(function () {
 			var currentLink = $(this);
 			var refElement = $(currentLink.attr("href"));
-			if (refElement.position().top - refElement.height() + (refElement.height()/3) <= scrollPosition && refElement.position().top + refElement.height() > scrollPosition) {
-				$('.sticky-nav ul li a').removeClass("active");
+			if (refElement.position().top - navbarHeight <= scrollPosition && refElement.position().top + refElement.height() > scrollPosition) {
+				$('.nav-highlight ul li a').removeClass("active");
 				currentLink.addClass("active");
 			}
 			else{
